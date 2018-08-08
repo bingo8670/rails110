c5c18a219fa4e1319fa5cf4998bf83a9b253b192
bingo8670 <307286130@qq.com>
2018-08-08 17:22:46 +0800

    CRUD

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,35 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+  def show
+    @group = Group.find(params[:id])
+  end
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update Sucecess"
+  end
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group delete"
+    redirect_to groups_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end
